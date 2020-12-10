e7a3e9c649f8a8416eecd5d3105fc872e5017766
UnAngelLily <unangellily@gmail.com>
2020-12-10 12:39:55 -0700

    completed simple calculator

--- main.rb
+++ main.rb
@@ -11,4 +11,10 @@ num_2 = gets.chomp
 puts "The first number multipled by the second number is #{num_1.to_i * num_2.to_i}"
 25.times { print "-"}
 puts
-puts "The first number divided by the second number is #{num_1.to_i % num_2.to_i}"
+puts "The first number divided by the second number is #{num_1.to_i / num_2.to_i}"
+25.times { print "-"}
+puts
+puts "The first number plus the second number is #{num_1.to_i + num_2.to_i}"
+25.times { print "-"}
+puts
+puts "The first number minus the second number is #{num_1.to_i - num_2.to_i}"
