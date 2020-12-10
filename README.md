80e6e62b5606c7e133b954c7eb355d99954b2bb2
UnAngelLily <unangellily@gmail.com>
2020-12-10 12:09:20 -0700

    started simple calculator

---
+++ README.md
@@ -0,0 +1,1 @@
+Start Simple calculator in terminal by inputing "Ruby main.rb"
---
+++ main.rb
@@ -0,0 +1,14 @@
+puts "Simple calculator"
+25.times { print "-"}
+puts
+puts "Enter the first number"
+num_1 = gets.chomp
+puts "Enter the second number"
+num_2 = gets.chomp
+# puts "The first number multipled by the second number is #{num_1 * num_2}"
+  #gets.chomp produces two strings and two strings cannot be multiple into an interger
+# can either change the varible above or the varible in the equation
+puts "The first number multipled by the second number is #{num_1.to_i * num_2.to_i}"
+25.times { print "-"}
+puts
+puts "The first number divided by the second number is #{num_1.to_i % num_2.to_i}"
