dd25d14fc87d4be6380c5bf9469f4ce8f3af1408
UnAngelLily <unangellily@gmail.com>
2020-12-15 08:44:46 -0700

    Finished simple calculator

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-puts "Simple calculator"
-25.times { print "-"}
+# puts "Simple calculator"
+# 25.times { print "-"}
 # puts
 # puts "Enter the first number"
 # num_1 = gets.chomp
@@ -19,25 +19,40 @@ puts "Simple calculator"
 # puts
 # puts "The first number minus the second number is #{num_1.to_i - num_2.to_i}"
 
+puts "Simple calculator"
+25.times { print "-"}
 puts
 puts "Please enter the first number"
-first_number = gets.chomp
+num_1 = gets.chomp
 puts "Please enter the second number"
-second_number = gets.chomp
+num_2 = gets.chomp
 puts "What do you want to do?"
 puts "\n
       Enter 1 for addtion,\n
       Enter 2 for subtraction,\n
-      Enter 3 for multiplcation,\n
+      Enter 3 for multiplication,\n
       Enter 4 for division.\n"
 user_entry = gets.chomp
 
 if user_entry == "1"
-  puts "You have choosen to add the values together."
+  puts "You have choosen to add the values together.\n
+  #{num_1.to_i + num_2.to_i}"
+  25.times { print "-"}
 elsif user_entry == "2"
-  puts "You have choosen to subtract the first value from the second value."
+  puts "You have choosen to subtract the first value from the second value.\n
+  #{num_1.to_i - num_2.to_i}"
+  25.times { print "-"}
 elsif user_entry == "3"
-  puts "You have choosen to multiply the values together."
+  puts "You have choosen to multiply the values together.\n
+  #{num_1.to_i * num_2.to_i}"
+  25.times { print "-"}
 elsif user_entry == "4"
-  puts "You have choosen to divide the first value by the second value."
+  puts "You have choosen to divide the first value by the second value.\n
+  #{num_1.to_i / num_2.to_i}"
+  25.times { print "-"}
+else
+  puts "Error\n
+  Must be an interger to be calculated on Simple calculator.\n
+  or\n
+  Must use selection of addition, subtraction, multiplication, or division (1,2,3, or 4)"
 end
